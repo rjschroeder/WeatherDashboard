9e1fc29bb894a2666a0ad316e86b7924d8e03906
rjschroeder <are.jay.schroeder@gmail.com>
2022-11-30 17:30:26 -0600

    added function to display search history

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,16 @@ let tempDisplaySpan = document.getElementById("tempDisplay");
 let windDisplaySpan = document.getElementById("windDisplay");
 let humidityDisplaySpan = document.getElementById("humidityDisplay");
 let forecastDiv = document.getElementById("forecastDiv");
+
+function displaySearchHistory() {
+    let prevSearches = JSON.stringify(window.localStorage.getItem("searchHistory")) || [];
+    searchHistoryButtonDiv.textContent = "";
+    for(let i = prevSearches.length; i < 0; i--){
+        let button = document.createElement("button");
+        button.setAttribute("type", "submit");
+        button.setAttribute("class", "btn btn-secondary btn-block")
+        button.textContent = prevSearches[i];
+        searchHistoryButtonDiv.appendChild(button);
+    }
+}
+displaySearchHistory();
